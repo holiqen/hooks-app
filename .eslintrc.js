9f245a394cd66febb26eec464b9ce663709389db
Artem <holiqen@yahoo.com>
2022-07-06 13:47:30 +0400

    project initialization

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks"],
+  rules: {
+    indent: "error",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+};
---
+++ makefile
@@ -0,0 +1,2 @@
+eslint:
+	npm run eslint
